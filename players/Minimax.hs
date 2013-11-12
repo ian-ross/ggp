3e293b8a4f91e1a9e4f472080a2952ddd20b3349
Ivan Glushkov <ivan.glushkov@gmail.com>
2013-11-13 01:53:03 +0400

    refactor minimax

--- players/Minimax.hs
+++ players/Minimax.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE RecordWildCards, TemplateHaskell #-}
 module Minimax (minimaxPlayer) where
 
+import Control.Monad (when)
 import Data.Function (on)
 import Data.List (intercalate, maximumBy, delete)
 import GGP.Player
@@ -9,65 +10,47 @@ import Language.GDL
 
 type Game a = GGP Int a
 
+data MoveType = Max | Min deriving (Show)
+
 modNest :: Int -> Game ()
 modNest delta = modify (\m -> m { matchExtra = matchExtra m + delta })
 
-boundedMinimum :: [Integer] -> Integer
-boundedMinimum [] = 100
-boundedMinimum (0:_) = 0
-boundedMinimum (x:xs) = x `min` boundedMinimum xs
-
-boundedMaximum :: [Integer] -> Integer
-boundedMaximum [] = 0
-boundedMaximum (100:_) = 100
-boundedMaximum (x:xs) = x `max` boundedMaximum xs
-
-minscore :: State -> Move -> Game Integer
-minscore st m = do
+makeMove :: MoveType -> State -> Game Integer
+makeMove t st = do
   Match {..} <- get
   logMsg $ (replicate matchExtra ' ') ++
-    "minscore: st=" ++ (intercalate ", " $ map prettyPrint st) ++
-    " m=" ++ prettyPrint m
-  let oppRole = head $ delete matchRole $ roles matchDB
-  let as = legal matchDB st oppRole
-      poss = map (\a -> applyMoves matchDB st [(matchRole, m), (oppRole, a)]) as
-  modNest 1
-  ss <- mapM maxscore poss
-  modNest (-1)
-  let retval = boundedMinimum ss
-  logMsg $ (replicate matchExtra ' ') ++ "minscore returns " ++ show retval
-  return retval
+    "makeMove " ++ show t ++ ": st=" ++ (intercalate ", " $ map prettyPrint st)
 
-maxscore :: State -> Game Integer
-maxscore st = do
-  Match {..} <- get
-  logMsg $ (replicate matchExtra ' ') ++
-    "maxscore: st=" ++ (intercalate ", " $ map prettyPrint st)
+  let 
+      oppRole = head $ delete matchRole $ roles matchDB
+      (nextt, findRes, role) = case t of
+            Max -> (Min, maximum, matchRole)
+            Min -> (Max, minimum, oppRole)
+      as = legal matchDB st role
+      poss = map (\a -> applyMoves matchDB st [(role, a)]) as
+
   retval <- if isTerminal matchDB st
             then return $ goal matchDB st matchRole
             else do
               modNest 1
-              let as = legal matchDB st matchRole
-                  poss = map (\a -> applyMoves matchDB st [(matchRole, a)]) as
-              ss <- if matchNRoles > 1
-                    then mapM (minscore st) as
-                    else mapM maxscore poss
+              ss <- mapM (makeMove nextt) poss
               modNest (-1)
-              return $ boundedMaximum ss
-  logMsg $ (replicate matchExtra ' ') ++ "maxscore returns " ++ show retval
+              when (matchExtra == 0) $ logMsg $ "Moves and values: " ++ show ss
+              return $ findRes ss
+
+  logMsg $ (replicate matchExtra ' ') ++ "makeMove " ++ show t ++ " returns " ++ show retval
   return retval
+
+
 
 bestMove :: State -> Game Move
 bestMove st0 = do
   Match {..} <- get
-  let as = legal matchDB st0 matchRole
-      poss = map (\a -> applyMoves matchDB st0 [(matchRole, a)]) as
-  vs <- if matchNRoles > 1
-        then mapM (minscore st0) as
-        else mapM maxscore poss
-  let avs = zip as vs
-  logMsg $ "Moves and values: " ++ show avs
-  return $ fst $ maximumBy (compare `on` snd) avs
+  let
+    as = legal matchDB st0 matchRole
+    poss = map (\a -> applyMoves matchDB st0 [(matchRole, a)]) as
+  ss <- mapM (makeMove Max) poss
+  return $ fst $ maximumBy (compare `on` snd) $ zip as ss
 
 minimaxPlayer :: Player Int
 minimaxPlayer = def { initExtra = const 0, handlePlay = basicPlay bestMove }
